a6ee60d0664e5b9483109519e8306ed415a3593d
Matthew Brett <matthew.brett@gmail.com>
2017-08-03 15:32:39 -0700

    Inital commit

---
+++ config.sh
@@ -0,0 +1,104 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+GEOS_VERSION="${GEOS_VERSION:-3.6.2}"
+PROJ_VERSION="${PROJ_VERSION:-4.9.3}"
+JASPER_VERSION="${JASPER_VERSION:-1.900.1.uuid}"
+GDAL_VERSION="${GDAL_VERSION:-2.2.1}"
+SQLITE3_VERSION="${SQLITE3_VERSION:-3200000}"
+JSON_C_VERSION="${JSON_C_VERSION:-0.12.1}"
+
+function build_geos {
+    build_simple geos $GEOS_VERSION http://download.osgeo.org/geos
+}
+
+function build_jasper {
+    if [ -e jasper-stamp ]; then return; fi
+    fetch_unpack http://download.osgeo.org/gdal/jasper-${JASPER_VERSION}.tar.gz
+    (cd jasper-${JASPER_VERSION} \
+        && ./configure --disable-debug --enable-shared --prefix=$BUILD_PREFIX \
+        && make \
+        && make install)
+    touch jasper-stamp
+}
+
+function build_proj {
+    build_simple proj $PROJ_VERSION http://download.osgeo.org/proj
+}
+
+function build_sqlite3 {
+    if [ -e sqlite3-stamp ]; then return; fi
+    fetch_unpack https://sqlite.org/2017/sqlite-autoconf-${SQLITE3_VERSION}.tar.gz 
+    (cd sqlite3-autoconf-${SQLITE3_VERSION} \
+        && ./configure --prefix=$BUILD_PREFIX \
+        && make \
+        && make install)
+    touch sqlite3-stamp
+}
+
+function build_json_c {
+    build_simple json-c $JSON_C_VERSION https://s3.amazonaws.com/json-c_releases/releases
+}
+
+function build_gdal {
+    if [ -e gdal-stamp ]; then return; fi
+    build_zlib
+    build_curl
+    build_json_c
+    build_geos
+    build_jpeg
+    build_png
+    build_jasper
+    build_proj
+    build_hdf5
+    fetch_unpack http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz
+    (cd gdal-${GDAL_VERSION} \
+        && ./configure --disable-debug \
+        --with-threads \
+        --disable-debug \
+        --disable-static \
+        --without-grass \
+        --without-libgrass \
+        --without-jpeg12 \
+        --with-jasper=$BUILD_PREFIX \
+        --with-libtiff=internal \
+        --with-jpeg \
+        --with-gif \
+        --with-png \
+        --with-geotiff=internal \
+        --with-sqlite3=$BUILD_PREFIX \
+        --with-pcraster=internal \
+        --with-pcraster=internal \
+        --with-pcidsk=internal \
+        --with-bsb \
+        --with-grib \
+        --with-pam \
+        --with-geos=$BUILD_PREFIX/bin/geos-config \
+        --with-static-proj4=$BUILD_PREFIX \
+        --with-expat=/usr \
+        --with-libjson-c \
+        --with-libiconv-prefix=/usr \
+        --with-libz \
+        --with-curl=curl-config \
+        --without-python \
+        --prefix=$BUILD_PREFIX \
+        && make \
+        && make install)
+    touch gdal-stamp
+}
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    build_gdal
+}
+
+function build_wheel {
+    build_pip_wheel gdal/swig/python
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    (cd ../gdal/autotest && python run_all.py)
+}
